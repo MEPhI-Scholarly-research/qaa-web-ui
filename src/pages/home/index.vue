6ba15f7c79f7087b18f015b5e7c40d5972d6cdc2
Daniil Shenyagin <d.shenyagin@tinkoff.ru>
2023-04-02 23:34:15 +0300

    feat: added ws

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,1 +1,28 @@
-<template>Home</template>
+<script lang="ts">
+import { getIO } from '@/shared/sockets'
+
+type Response = {
+  id: string
+  active: boolean
+}
+
+const { socket, connected } = await getIO()
+
+socket.emit('quiz.content', { id: undefined })
+
+socket.on('quiz.content', (data: Response) => {
+  console.log({ data })
+})
+
+export default {
+  data() {
+    return {
+      connected: connected
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Connected: {{ connected }}</h1>
+</template>
