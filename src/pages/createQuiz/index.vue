b70cc77bbb285cd8efc7b5a156a726d0314fd1dd
Daniil Shenyagin <d.shenyagin@tinkoff.ru>
2023-05-21 09:53:05 +0300

    start make construction

---
+++ src/pages/createQuiz/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="quizCreatorWrapper"></div>
+</template>
+
+<!-- 
+  TODO: 
+    - common
+      - title
+      - description
+      - time_limit
+    - constructor
+      - list questions
+    - stepper styles
+    - query params
+    - api integration
+ -->
+
+<script lang="ts">
+export default {
+  name: 'QuizCreator',
+  data() {
+    return {
+      currentStep: 'common'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.quizCreatorWrapper {
+}
+</style>
---
+++ src/pages/createQuiz/stepper.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="steps">
+    <div
+      class="step"
+      v-for="(step, index) in steps"
+      @click="() => onChangeStep && onChangeStep(step.value)"
+      :key="step.value"
+    >
+      <div class="index">{{ index }}</div>
+      <div class="label">{{ step.title }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import type { PropType } from 'vue'
+
+type Props = {
+  steps: {
+    title: string
+    value: string
+  }[]
+  onChangeStep: (step: string) => void
+}
+
+export default {
+  name: 'Stepper',
+  props: {
+    steps: Object as PropType<Props['steps']>,
+    onChangeStep: Object as PropType<Props['onChangeStep']>
+  }
+}
+</script>
